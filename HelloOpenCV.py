9a876a1568de468323e3c9c2cef1708f7c246369
bowsandrrows <154934324+bowsandrrows@users.noreply.github.com>
2024-01-13 15:49:59 +0100

    Update HelloOpenCV.py

--- HelloOpenCV.py
+++ HelloOpenCV.py
@@ -9,7 +9,7 @@ tx_font = cv2.FONT_HERSHEY_COMPLEX
 tx_color = (255, 255., 0)
 tx_fontScale = 2
 tx_thickness = 2
-cv2.putText(img, "Hello OpenCV", tx_start, tx_font, tx_fontScale, tx_color, tx_thickness)  # has to be in this order! ?
+cv2.putText(img, "Hello OpenCV", tx_start, tx_font, tx_fontScale, tx_color, tx_thickness)  # has to be in this order...?
 
 cv2.imshow('hello', img)
 cv2.waitKey(3000)
